3d4a72964ca0dfe740833a6f4599c57f3f72d4b6
abhishek.singh@agileapt.com <abhishek.singh@agileapt.com>
2024-08-12 01:13:15 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
+// pages/index.js
 import Head from 'next/head';
-import ChatbotFullScreen from '../components/ChatBotFullscreen';
-
+import Chatbot from '../components/Chatbot';
 
 export default function Home() {
   return (
-    <>
-         <ChatbotFullScreen />
-    </>
+    <div>
+      <Head>
+        <title>Abhishek Singh's Portfolio</title>
+        <meta name="description" content="Abhishek Singh's Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to My Portfolio</h1>
+        {/* Other content */}
+      </main>
+
+      <Chatbot /> {/* Chatbot is always visible */}
+    </div>
   );
 }
